562e349f0fe0d3cb680dac2d3931b0a7f050977c
aranhasete <aranhajba@gmail.com>
2017-05-28 23:21:50 -0300

    Inicial

---
+++ Assets/Scripts/CameraControle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControle : MonoBehaviour {
+
+    public Transform alvo;      //Quem a camera segue
+    public float velocidade;    //Com qual velocidade a camera segue
+
+    public float minX = -9999;
+    public float maxX = 9999;
+    public float minY = -9999;
+    public float maxY = 9999;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (alvo != null)
+        {
+            var destino = new Vector3(alvo.position.x, alvo.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, destino, velocidade * Time.deltaTime);
+        }
+
+        //Limite
+        var camera = GetComponent<Camera>();
+        if (camera.orthographic)
+        {
+            var x = transform.position.x;
+            var y = transform.position.y;
+            var z = transform.position.z;
+
+            var alcanceHorizontal = camera.orthographicSize * Screen.width / Screen.height;
+
+            var realMinX = minX + alcanceHorizontal;
+            var realMaxX = maxX - alcanceHorizontal;
+
+            x = Mathf.Clamp(x, realMinX, realMaxX);
+            y = Mathf.Clamp(y, minY, maxY);
+
+            transform.position = new Vector3(x, y, z);
+        }
+    }
+}
---
+++ Assets/Scripts/Vilao.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vilao : MonoBehaviour {
+
+
+	public float vel = 1.0f;
+	public bool liberaPer = false;
+	public float distancia;
+	public Transform Hero;
+	public bool face = true;
+	public Animator vilao;
+
+	void Start () {
+
+
+		vilao = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Hero != null)
+        {
+            distancia = Vector2.Distance(transform.position, Hero.transform.position);
+
+            //Flip
+
+            if ((Hero.transform.position.x > this.transform.position.x) && !face)
+            {
+                flip();
+            }
+            else if ((Hero.transform.position.x < this.transform.position.x) && face)
+            {
+
+                flip();
+            }
+
+
+            if ((liberaPer == true) && distancia > 2.8f)
+            {
+
+                vilao.SetBool("InimigoAndar", true);
+                vilao.SetBool("InimigoIdle", false);
+
+                if (Hero.transform.position.x < this.transform.position.x)
+                {
+                    transform.Translate(new Vector2(-vel * Time.deltaTime, 0));
+
+                }
+                else if (Hero.transform.position.x > this.transform.position.x)
+                {
+                    transform.Translate(new Vector2(vel * Time.deltaTime, 0));
+
+                }
+            }
+            else
+            {
+
+                vilao.SetBool("InimigoAndar", false);
+                vilao.SetBool("InimigoIdle", true);
+            }
+        }
+	}
+
+
+	void flip()
+	{
+
+		face = !face;
+
+		Vector3 scala = this.transform.localScale;
+		scala.x *= -1;
+		this.transform.localScale = scala;
+	}
+
+	void OnTriggerEnter2D(Collider2D outro)
+	{
+		if(outro.gameObject.CompareTag("Player"))
+		{
+			liberaPer = true;
+            Hero = outro.gameObject.GetComponent<Transform>();
+        }
+	}
+
+}
